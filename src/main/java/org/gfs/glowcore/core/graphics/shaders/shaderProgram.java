82e09d5c008a3c5b60d3702a1b318df711fd9929
LISis3 <97257031+BronzeCode@users.noreply.github.com>
2025-07-22 06:05:36 +0500

    chore: исправлена пустота в  shaderProgram.java

---
+++ src/main/java/org/gfs/glowcore/core/graphics/shaders/shaderProgram.java
@@ -0,0 +1,56 @@
+package org.gfs.glowcore.core.graphics.shaders;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class shaderProgram {
+    
+    private int id;
+   
+    
+    private shader attachedShader;
+    
+    public shaderProgram() {
+        id = glCreateProgram();
+    }
+    
+    public void attachShader(shader shader) {
+        glAttachShader(id, shader.getID());
+        this.attachedShader = shader; // Сохраняем ссылку на прикрепленный шейдер
+    }
+    
+    public void link() {
+        glLinkProgram(id);
+        checkStatus();
+    }
+    
+    public void use() {
+        glUseProgram(id);
+    }
+    
+    public void delete() {
+        glDeleteProgram(id);
+    }
+    
+    public void terminateShaders() {
+        
+        if (attachedShader != null) {
+            glDeleteShader(attachedShader.getID());
+        }
+    }
+    
+    public void terminateShaderProgram() {
+        terminateShaders();
+        delete();
+    }
+    
+    public int getID() {
+        return id;
+    }
+    
+    public void checkStatus() {
+        int status = glGetProgrami(id, GL_LINK_STATUS);
+        if (status != GL_TRUE) {
+            throw new RuntimeException(glGetProgramInfoLog(id));
+        }
+    }
+}
